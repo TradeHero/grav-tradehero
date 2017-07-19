bc86a9c9fb9cae29782938d8118320c731580a56
viraj2252 <viraj2252@gmail.com>
2017-07-19 11:43:19 +0800

    stop showint toggle button on faq

--- user/themes/vjtest/js/main.js
+++ user/themes/vjtest/js/main.js
@@ -8,27 +8,27 @@
 
     //app banner
 
-    new SmartBanner({
-        daysHidden: 5, // days to hide banner after close button is clicked (defaults to 15)
-        daysReminder: 9, // days to hide banner after "VIEW" button is clicked (defaults to 90)
-        appStoreLanguage: 'us', // language code for the App Store (defaults to user's browser language)
-        title: 'TradeHero',
-        author: 'Tradehero',
-        button: 'VIEW',
-        store: {
-            ios: 'On the App Store',
-            android: 'In Google Play',
-            windows: 'In Windows store'
-        },
-        price: {
-            ios: 'FREE',
-            android: 'FREE',
-            windows: 'FREE'
-        }
-        // , theme: '' // put platform type ('ios', 'android', etc.) here to force single theme on all device
-        // , icon: '' // full path to icon image if not using website icon image
-        // , force: 'ios' // Uncomment for platform emulation
-    });
+    // new SmartBanner({
+    //     daysHidden: 5, // days to hide banner after close button is clicked (defaults to 15)
+    //     daysReminder: 9, // days to hide banner after "VIEW" button is clicked (defaults to 90)
+    //     appStoreLanguage: 'us', // language code for the App Store (defaults to user's browser language)
+    //     title: 'TradeHero',
+    //     author: 'Tradehero',
+    //     button: 'VIEW',
+    //     store: {
+    //         ios: 'On the App Store',
+    //         android: 'In Google Play',
+    //         windows: 'In Windows store'
+    //     },
+    //     price: {
+    //         ios: 'FREE',
+    //         android: 'FREE',
+    //         windows: 'FREE'
+    //     }
+    //     // , theme: '' // put platform type ('ios', 'android', etc.) here to force single theme on all device
+    //     // , icon: '' // full path to icon image if not using website icon image
+    //     // , force: 'ios' // Uncomment for platform emulation
+    // });
 
     // resize google map on active tab    
 
